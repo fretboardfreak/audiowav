fabcd6ff0b2576a9e4cc55e078f86683e62de9b2
Curtis Sand <curtis@curtissand.com>
2019-02-18 12:47:18 -0800

    main: remove basic tests

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,10 @@
 
 int main(int argc, char **argv){
     std::cout << "audiowav" << std::endl;
-    WaveGenerator *generator = new WaveGenerator();
 
 #ifdef DEBUG
     std::cout << "I was compiled with DEBUG defined." << std::endl;
 #endif
 
-    std::cout << generator->genSamples(440, 1000) << std::endl;
     return 0;
 }
