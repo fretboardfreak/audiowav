aa9ec488112a2f7170f624060970bc096cd3929d
Curtis Sand <curtis@curtissand.com>
2019-02-23 07:30:13 -0800

    work on fleshing out the WaveGenerator interface

--- src/wavegen.cpp
+++ src/wavegen.cpp
@@ -15,6 +15,8 @@
  */
 #include "wavegen.hpp"
 #include <iostream>
+
+#define _USE_MATH_DEFINES
 #include <cmath>
 
 /* Calculate number of frames given sample length in seconds.
@@ -27,4 +29,31 @@ int seconds_to_frames(float seconds, int framerate){
  * seconds = framecount / framerate = (frames) / (frames / seconds) */
 float frames_to_seconds(int framecount, int framerate){
   return (float) framecount / (float) framerate;
+}
+
+
+WaveGenerator::WaveGenerator(int framerate, int fade_percentage){}
+
+/* Generate a waveform at a constant frequency. */
+void WaveGenerator::constant(int frequency, float *channel, int frame_count){
+}
+
+/* Generate a waveform of linearly changeng frequency.
+ * note: only end frequency is needed because current fruency is the start.*/
+void WaveGenerator::linear(int end_frequency, float *channel, int frame_count){
+}
+
+/* Reset the WaveGenerator to start a new waveform.*/
+bool WaveGenerator::reset(void){
+  return true;
+}
+
+/* Calculate the sinusoid phase angle for a given frame and frequency. */
+float WaveGenerator::sinusoid_angle(int frame, float frequency){
+  return 0;
+}
+
+/* Calculate the value of a sinusoid wave at given frame and frequency. */
+float WaveGenerator::sinusoid_value(int frame, float frequency){
+  return 0;
 }
--- src/wavegen.hpp
+++ src/wavegen.hpp
@@ -16,22 +16,32 @@
 #ifndef WAVEGEN_HPP
 #define WAVEGEN_HPP
 
+#include <complex>
+
 #define DEFAULT_FRAMERATE 44100
 
 /* Calculate number of frames given sample length in seconds. */
-int seconds_to_frames(float /*seconds*/, int /*framerate*/);
+int seconds_to_frames(float seconds, int framerate);
 /* Calculate length of sample given the framecount and framerate. */
-float frames_to_seconds(int /*frame_count*/, int /*framerate*/);
+float frames_to_seconds(int frame_count, int framerate);
 
 class WaveGenerator {
-  int framerate;
 public:
-  void constant(int /*frequency*/, float* /*channel*/, int /*frame_count*/);
+  /* Constructor */
+  WaveGenerator(int framerate, int fade_percentage);
+  /* Generate a waveform at a constant frequency. */
+  void constant(int frequency, float *channel, int frame_count);
+  /* Generate a waveform of linearly changeng frequency.
+   * note: only end frequency is needed because current fruency is the start.*/
+  void linear(int end_frequency, float *channel, int frame_count);
+  /* Reset the WaveGenerator to start a new waveform.*/
+  bool reset(void);
 protected:
+  int fade_percentage, framerate, last_phase;
   /* Calculate the sinusoid phase angle for a given frame and frequency. */
-  float sinusoid_angle(int /*frame*/, float /*frequency*/);
+  float sinusoid_angle(int frame, float frequency);
   /* Calculate the value of a sinusoid wave at given frame and frequency. */
-  float sinusoid_value(int /*frame*/, float /*frequency*/);
+  float sinusoid_value(int frame, float frequency);
 };
 
 
